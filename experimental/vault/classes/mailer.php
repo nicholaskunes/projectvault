f864e694fc24731deba4915c1529d3402ff86aca
nicholaskunes <kunes.nick0@gmail.com>
2017-12-05 13:49:05 -0800

    commit

--- experimental/vault/classes/mailer.php
+++ experimental/vault/classes/mailer.php
@@ -19,7 +19,7 @@ require 'SMTP.php';
                             $mail->isSMTP(); // Set mailer to use SMTP
                             $mail->Host       = 'vps160551.vps.ovh.ca'; // Specify main and backup SMTP servers
 
-                            $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+                            $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
                             $mail->Port       = 587; // TCP port to connect to
 							                            
                             //Recipients
