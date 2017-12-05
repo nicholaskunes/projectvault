98a17bc9a62c6aec9ce7526d1209fe82a2e34273
nicholaskunes <kunes.nick0@gmail.com>
2017-12-04 21:39:59 -0800

    commit

--- experimental/vault/classes/Registration.php
+++ experimental/vault/classes/Registration.php
@@ -37,23 +37,23 @@ class Registration
     private function registerNewUser()
     {
         if (empty($_POST['first'])) {
-            $this->errors[] = "Empty Username";
+            echo "Empty Username";
         } elseif (empty($_POST['passwd']) || empty($_POST['passwd2'])) {
-            $this->errors[] = "Empty Password";
+            echo "Empty Password";
         } elseif ($_POST['passwd'] !== $_POST['passwd2']) {
-            $this->errors[] = "Password and password repeat are not the same";
+            echo "Password and password repeat are not the same";
         } elseif (strlen($_POST['passwd']) < 6) {
-            $this->errors[] = "Password has a minimum length of 6 characters";
+            echo "Password has a minimum length of 6 characters";
         } elseif (strlen($_POST['first']) > 64 || strlen($_POST['first']) < 2) {
-            $this->errors[] = "Username cannot be shorter than 2 or longer than 64 characters";
+            echo "Username cannot be shorter than 2 or longer than 64 characters";
         } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['first'])) {
-            $this->errors[] = "Username does not fit the name scheme: only a-Z and numbers are allowed, 2 to 64 characters";
+            echo "Username does not fit the name scheme: only a-Z and numbers are allowed, 2 to 64 characters";
         } elseif (empty($_POST['email'])) {
-            $this->errors[] = "Email cannot be empty";
+            echo "Email cannot be empty";
         } elseif (strlen($_POST['email']) > 64) {
-            $this->errors[] = "Email cannot be longer than 64 characters";
+            echo "Email cannot be longer than 64 characters";
         } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $this->errors[] = "Your email address is not in a valid email format";
+            echo "Your email address is not in a valid email format";
         } elseif (!empty($_POST['first'])
             && strlen($_POST['first']) <= 64
             && strlen($_POST['first']) >= 2
@@ -70,7 +70,7 @@ class Registration
 
             // change character set to utf8 and check it
             if (!$this->db_connection->set_charset("utf8")) {
-                $this->errors[] = $this->db_connection->error;
+                echo $this->db_connection->error;
             }
 
             // if no connection errors (= working database connection)
@@ -92,7 +92,7 @@ class Registration
                 $query_check_user_name = $this->db_connection->query($sql);
 
                 if ($query_check_user_name->num_rows == 1) {
-                    $this->errors[] = "Sorry, that username / email address is already taken.";
+                    echo "Sorry, that username / email address is already taken.";
                 } else {
                     // write new user's data into database
                     $sql = "INSERT INTO users (first, passwdhash, email)
@@ -103,14 +103,14 @@ class Registration
                     if ($query_new_user_insert) {
                         $this->messages[] = "Your account has been created successfully. You can now log in.";
                     } else {
-                        $this->errors[] = "Sorry, your registration failed. Please go back and try again.";
+                        echo "Sorry, your registration failed. Please go back and try again.";
                     }
                 }
             } else {
-                $this->errors[] = "Sorry, no database connection.";
+                echo "Sorry, no database connection.";
             }
         } else {
-            $this->errors[] = "An unknown error occurred.";
+            echo "An unknown error occurred.";
         }
     }
 }
