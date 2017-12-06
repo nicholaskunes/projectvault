ffe772989b31998c796c739e4c3623a1b95c2612
nicholaskunes <kunes.nick0@gmail.com>
2017-12-05 20:45:32 -0800

    commit

--- experimental/vault/classes/Vault.php
+++ experimental/vault/classes/Vault.php
@@ -1,9 +1,38 @@
 <?php
 
-class WalletResponse {
-    public $guid;                       // string
-    public $address;                    // string
-    public $label;                      // string
+class WalletResponse
+{
+    public $guid; // string
+    public $address; // string
+    public $label; // string
+}
+
+
+if (isset($_POST["balance"])) {
+	$db_connection1 = null;
+    $db_connection1 = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
+    $db_connection1->set_charset("utf8");
+    if (!$db_connection1->connect_errno) {
+        
+        $email = $_SESSION["email"];
+        
+        $sql          = "SELECT wallet
+                        FROM users
+                        WHERE email = '" . $email . "';";
+        $wallet_check = $db_connection1->query($sql);
+        
+        if ($wallet_check->num_rows == 1) {
+            $result_row = $wallet_check->fetch_object();
+            if ($result_row->wallet != '') {
+                $Blockchain = new \Blockchain\Blockchain("fedcfc00-371d-4b84-b055-7052a4fb5cea");
+                $Blockchain->setServiceUrl("http://localhost:3030");
+                $address = $Blockchain->Wallet->getAddressBalance($result_row->wallet);
+                
+                echo $address->balance;
+            }
+        }
+    }
 }
 
 class Vault
@@ -19,50 +48,22 @@ class Vault
             
             $email = $db_connection->real_escape_string($email);
             
-            $sql     = "SELECT wallet
+            $sql          = "SELECT wallet
                         FROM users
                         WHERE email = '" . $email . "';";
             $wallet_check = $db_connection->query($sql);
             
             if ($wallet_check->num_rows == 1) {
                 $result_row = $wallet_check->fetch_object();
-				if($result_row->wallet == '') {
-					$Blockchain = new \Blockchain\Blockchain("fedcfc00-371d-4b84-b055-7052a4fb5cea");
-					$Blockchain->setServiceUrl("http://localhost:3030");
-					$wallet = $Blockchain->Create->create($passwd);
-					
-					$sql = "UPDATE users SET wallet='" . $wallet->address . "', guid='" . $wallet->guid . "' WHERE email='" . $email . "'";
+                if ($result_row->wallet == '') {
+                    $Blockchain = new \Blockchain\Blockchain("fedcfc00-371d-4b84-b055-7052a4fb5cea");
+                    $Blockchain->setServiceUrl("http://localhost:3030");
+                    $wallet = $Blockchain->Create->create($passwd);
+                    
+                    $sql                   = "UPDATE users SET wallet='" . $wallet->address . "', guid='" . $wallet->guid . "' WHERE email='" . $email . "'";
                     $query_new_user_insert = $db_connection->query($sql);
-				}
-            }
-        }
-    }
-	
-	public function checkBalance()
-    {
-        $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        
-        $db_connection->set_charset("utf8");
-        if (!$db_connection->connect_errno) {
-            
-            $email = $_SESSION["email"];
-            
-            $sql     = "SELECT wallet
-                        FROM users
-                        WHERE email = '" . $email . "';";
-            $wallet_check = $db_connection->query($sql);
-            
-            if ($wallet_check->num_rows == 1) {
-                $result_row = $wallet_check->fetch_object();
-				if($result_row->wallet != '') {
-					$Blockchain = new \Blockchain\Blockchain("fedcfc00-371d-4b84-b055-7052a4fb5cea");
-					$Blockchain->setServiceUrl("http://localhost:3030");
-					$address = $Blockchain->Wallet->getAddressBalance($result_row->wallet);
-					
-					return $address->balance;
-				}
+                }
             }
         }
     }
 }
-
