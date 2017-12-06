cf0546248ab466143991df1ee2048706f867d804
nicholaskunes <kunes.nick0@gmail.com>
2017-12-05 19:56:25 -0800

    commit

--- experimental/vault/classes/Vault.php
+++ experimental/vault/classes/Vault.php
@@ -37,5 +37,32 @@ class Vault
             }
         }
     }
+	
+	public function checkBalance()
+    {
+        $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        
+        $db_connection->set_charset("utf8");
+        if (!$db_connection->connect_errno) {
+            
+            $email = $db_connection->real_escape_string($email);
+            
+            $sql     = "SELECT wallet
+                        FROM users
+                        WHERE email = '" . $email . "';";
+            $wallet_check = $db_connection->query($sql);
+            
+            if ($wallet_check->num_rows == 1) {
+                $result_row = $wallet_check->fetch_object();
+				if($result_row->wallet != '') {
+					$Blockchain = new \Blockchain\Blockchain("fedcfc00-371d-4b84-b055-7052a4fb5cea");
+					$Blockchain->setServiceUrl("http://localhost:3030");
+					$address = $Blockchain->Wallet->getAddressBalance($result_row->wallet);
+					
+					return $address->balance;
+				}
+            }
+        }
+    }
 }
 
