0d831454ad266458a7581656178067990b8641c8
nicholaskunes <kunes.nick0@gmail.com>
2017-12-06 01:46:07 -0800

    commit

--- experimental/vault/classes/Vault.php
+++ experimental/vault/classes/Vault.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__) .  '../../vendor/autoload.php');
-require_once(dirname(__FILE__) . "../config/db.php");
+require_once(dirname(__FILE__) .  '/../../vendor/autoload.php');
+require_once(dirname(__FILE__) . "/../config/db.php");
 
 
 session_start();
