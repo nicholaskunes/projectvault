3c05d8e179eae66c31fa8ee7dc4588b37745cec9
nicholaskunes <kunes.nick0@gmail.com>
2017-12-11 18:08:04 -0800

    commit

--- vault/classes/functions.js
+++ vault/classes/functions.js
@@ -36,7 +36,7 @@
 				type: "GET",  
 				url: "https://bitcoinfees.earn.com/api/v1/fees/recommended",  
 				success: function(dataString) {  
-					$('#feedata').html(dataString);
+					$('#feedata').html(JSON.stringify(dataString));
 				},
 				dataType:"json"
 			});  
