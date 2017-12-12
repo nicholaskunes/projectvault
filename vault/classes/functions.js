a817ad1777923f66a15212556e1e626bd51ef704
nicholaskunes <kunes.nick0@gmail.com>
2017-12-11 18:56:17 -0800

    commit

--- vault/classes/functions.js
+++ vault/classes/functions.js
@@ -26,7 +26,7 @@
 				url: "vault/classes/Vault.php",  
 				data: {'btcprice': 'btcprice'},  
 				success: function(dataString) {  
-					$('#btccalcprice').html("BTC " + bcdiv(document.getElementById('amountinput').innerHTML, dataString, 5));
+					$('#btccalcprice').html("BTC " + parseFloat(document.getElementById('amountinput').innerHTML, dataString).toPrecision(5));
 				}
 			}); 
 		}
